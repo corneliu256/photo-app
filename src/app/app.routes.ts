3595523820bc21b4ae08c8658c67a041b3b7011b
Anghel Corneliu <anghelcorneliu256@yahoo.com>
2025-06-09 06:56:58 +0300

    clean up test.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'unsplash',
-        // component: PhotoGalleryPageComponent     
         loadComponent: () => import('./pages/photo-gallery-page/photo-gallery-page.component').then(m => m.PhotoGalleryPageComponent)    
     },  
     {
         path: 'personal',
-        // component: PersonalWebsiteComponent     
         loadComponent: () => import('./components/personal-website/personal-website.component').then(m => m.PersonalWebsiteComponent)    
     }, 
 ];
